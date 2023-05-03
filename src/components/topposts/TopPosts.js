063372cb90db8deae09ab41775e87fe360daaa93
RafaelPapa.dev <rafanicolasp@hotmail.com>
2023-05-03 10:00:42 -0300

    implemetar o mecanismo de pesquisa

--- src/components/topposts/TopPosts.js
+++ src/components/topposts/TopPosts.js
@@ -7,18 +7,20 @@ function TopPosts() {
   useEffect(() => {
     fetch('https://www.reddit.com/r/popular.json')
       .then(response => response.json())
-      .then(data => setPosts(data.data.children.slice(0, 10).map(child => child.data)))
-      .catch(error => console.error(error));
+      .then(data => {
+        setPosts(data.data.children);
+      });
   }, []);
 
   return (
-    <div className="posts">
-      {posts.map(post => (
-        <div key={post.id} className='post-border'>
-          <h2>{post.title}</h2>
-          <img src={post.thumbnail} alt={post.title} /><hr></hr>
-          <p>Posted by {post.author} on {new Date(post.created_utc * 1000).toLocaleString()}</p>
-          <p>{post.num_comments} comments</p><br></br>
+    <div className="top-posts">
+      {posts.map((post, index) => (
+        <div key={index} className="post">
+          <h3>{post.data.title}</h3>
+          <img src={post.data.thumbnail} alt="thumbnail" />
+          <p>Author: {post.data.author}</p>
+          <p>Date: {new Date(post.data.created_utc * 1000).toLocaleString()}</p>
+          <p>Comments: {post.data.num_comments}</p>
         </div>
       ))}
     </div>
